77cb95ddddfb88cc3808ce9aac8c585f40528f36
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2023-01-30 17:32:33 -0300

    Router funcional

---
+++ src/views/contacto.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Contacto = () => {
+    return (
+        <header className="App-header">
+        
+        <p>
+          Contact
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    )
+}
+
+export default Contacto;
